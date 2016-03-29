20deae45e83a1367bf6b20aedb774b4f4c3a8d51
Ariel Weher <ariel@weher.net>
2016-03-29 13:35:37 -0300

    Update backup-mysql.pl

--- backup-mysql.pl
+++ backup-mysql.pl
@@ -48,12 +48,12 @@ my $cpprogram = '/bin/cp';
 chomp $password;
 
 # Defino el programa que voy a usar para obtener la copia de la base de datos y sus parametros de funcionamiento
-my $parametros = "-h$host -u$usuario -p$password --flush-logs --opt --single-transaction --hex-blob";
+my $parametros = "-h$host -u$usuario -p$password --flush-logs --opt --add-drop-database --hex-blob --routines --triggers --events --single-transaction";
 
 # Obtengo todas las bases del server
 
 my @bases = ();
-my @lineas = `$mysqlprogram -h $host -u$usuario -p$password -e "show databases" --batch`;
+my @lineas = `$mysqlprogram -h $host -u$usuario -p$password -e "SHOW DATABASES" --batch`;
 
 my $n = 0;
 foreach (@lineas){
@@ -61,34 +61,39 @@ foreach (@lineas){
         chomp;
         next if ($_ =~ /information_schema/);
         next if ($_ =~ /performance_schema/);
-        
+        print "Encontre la base: $_\n" if $DEBUG;
         push(@bases, $_) unless $n eq 1;
 }
 
 # Obtengo la fecha en este formato: 20070606-120645
 my $fechahora = `$dateprogram +%Y%m%d-%H%M%S%s`;
 chomp $fechahora;
+print "La variable \$fechahora es: $fechahora\n" if $DEBUG;
 
 my $directorio = $dirdestino.$fechahora.'.borrame/';
 my $existeeldir = 0;
 
+##### Parche para ocupar menos espacio..
+#@bases = ();
+#@bases = qw /database1 database2 database5/;
+
 # Extraigo las bases...
 foreach my $base (@bases){
-        print "Making Backup of Base: $base ($fechahora)...\n";
+        print "Making Backup of Base: $base ($fechahora)...\n if $DEBUG";
 
         # Creo el directorio si es que no existe...
         if (!$existeeldir){
-                my $tmpcmd = `$mkdirprogram $directorio`;
+                my $tmpcmd = `$mkdirprogram $directorio` if not $READONLY;
                 $existeeldir = 1;
         }
         my $tmparchivo = $directorio.$base.'.sql';
-        my $tmpcmd = `$dumpprogram $parametros $base > $tmparchivo`;
+        my $tmpcmd = `$dumpprogram $parametros $base > $tmparchivo` if not $READONLY;
         $fechahora = `/bin/date +%Y%m%d-%H%M%S-%s`;
         chomp $fechahora;
 }
 
 # Comprimo el archivo usando bzip2
-print "Compressing temp dir ($directorio)\n";
+print "Compressing temp dir ($directorio)\n" if $DEBUG;
 
 $fechahora = `$dateprogram +%Y%m%d-%H%M%S-%s`;
 chomp $fechahora;
@@ -101,18 +106,18 @@ if ($cantarchivos > $numbackup){
         # Obtengo el archivo mas viejo del directorio
         my $archivomasviejo = `$lsprogram -t -r -1 $dirdestino | $headprogram -1`;
         chomp $archivomasviejo;
-        my $jobh = `$rmprogram -rf $archivomasviejo`;
+        my $jobh = `$rmprogram -rf $archivomasviejo` if not $READONLY;
 }
 
 my $archivodestino = $dirdestino."bases.$fechahora.tar.bz2";
 chomp $archivodestino;
 
 my $tmpcmd = `$tarprogram cvfj $archivodestino $directorio`;
-print "\nI have created the file $archivodestino\n";
-print "Cleaning temp dirs...\n";
+print "\nI have created the file $archivodestino\n" if $DEBUG;
+print "Cleaning temp dirs...\n" if $DEBUG;
 $tmpcmd = `$rmprogram -rf $directorio` if ($directorio =~ /.*borrame.*/);
-$tmpcmd = `$rmprogram -rf $dirdestino$linksimbolico`;
-$tmpcmd = `$lnprogram -s $archivodestino "$dirdestino$linksimbolico"`;
-print"Have a nice day...\n";
+$tmpcmd = `$rmprogram -rf $dirdestino$linksimbolico` if not $READONLY;
+$tmpcmd = `$lnprogram -s $archivodestino "$dirdestino$linksimbolico"` if not $READONLY;
+print"Have a nice day...\n" if $DEBUG;
 
 exit 0;
